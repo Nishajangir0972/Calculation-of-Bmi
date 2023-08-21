92161f30c333c3cd275e151e15442d991fc7624f
Nishajangir0972 <nishajangir9302@gmail.com>
2023-08-20 18:39:42 -0700

    main

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+// import Counter from './Counter.jsx'
+// import Color from './Color.jsx'
+import Bmi from './Bmi.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <Color />
+  <Bmi/>
+  
+)
