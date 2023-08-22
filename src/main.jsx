641e02ff7ac581d18fe12a54a2c450f3f394650e
Nishajangir0972 <nishajangir9302@gmail.com>
2023-08-22 12:02:19 -0700

    first

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+// import './index.css'
 // import Counter from './Counter.jsx'
 // import Color from './Color.jsx'
-import Bmi from './Bmi.jsx'
+// import Bmi from './Bmi.jsx'
+import Todo from './Todo.jsx'
+import './todo.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <Color />
-  <Bmi/>
+  // <Bmi/>
+  <Todo/>
   
 )
